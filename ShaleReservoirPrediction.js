d31cf757e2a6557fb407b875ee750287a00e2ec8
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2019-07-04 16:14:18 -0500

    Update ShaleReservoirPrediction.js

--- ShaleReservoirPrediction.js
+++ ShaleReservoirPrediction.js
@@ -106,7 +106,7 @@ class ShaleReservoirProductionPerformance
     {
         //note: the abstraction in this method is simplified and similar to sklearn's MLPRegressor(args),
         //    : such that calling the modelingOption (DNN) is reduced to just 2 lines of statements
-        //    : see testProductionPerformace() method below - lines 312 and 314
+        //    : e.g. see testProductionPerformace() method below - lines 345 and 347
         
         if(this.modelingOption === "dnn")
         {
