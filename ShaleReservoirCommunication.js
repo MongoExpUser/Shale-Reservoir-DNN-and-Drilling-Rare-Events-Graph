827114d4f6a34b32b0e2810a77a39e83480b19d4
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2019-07-04 00:40:08 -0500

    Update ShaleReservoirCommunication.js

--- ShaleReservoirCommunication.js
+++ ShaleReservoirCommunication.js
@@ -1,3 +1,23 @@
+/* @License Starts
+ *
+ * Copyright © 2015 - present. MongoExpUser
+ *
+ * License: MIT - See: https://github.com/MongoExpUser/Shale-Reservoir-DNN/blob/master/LICENSE
+ *
+ * @License Ends
+ *
+ *
+ * ...Ecotert's ShaleReservoirCommuication.js (released as open-source under MIT License) implements:
+ *
+ *
+ * Relevant communications methods - e.g. (1) read/write files on disk (2) connect to MongoDB (3) read read/write files from MongoDB (4) etc.
+ *
+ *
+ *
+ */
+ 
+ 
+ 
 class ShaleReservoirCommuication
 {
     constructor()
