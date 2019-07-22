e92fc55a6d6567dbba1ff5a004158d8a7b1e97e4
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2019-07-21 19:38:43 -0500

    Update test_addon.cc

--- test_addon.cc
+++ test_addon.cc
@@ -2,7 +2,7 @@
  *
  * Copyright © 2015 - present. MongoExpUser
  *
- * License: MIT - See: https://github.com/MongoExpUser/Heavy-Oil-PVT-Simulator/blob/master/LICENSE
+ * License: MIT - See: https://github.com/MongoExpUser/Shale-Reservoir-DNN/blob/master/LICENSE
  *
  * @License Ends
  *
