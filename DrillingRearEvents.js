8161f087773163919da754f2dc502d515e844c42
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2019-08-18 03:28:49 -0500

    Create DrillingRearEvents.js

---
+++ DrillingRearEvents.js
@@ -0,0 +1,50 @@
+/5. DrillingRearEvents for gitHub
+/*
+ * Ecotert's DrillingRearEvents .js (released as open-source under MIT License) implements:
+ *
+ * Drilling Rear Events or Anomalies (vibration, kick and stuckpipe) detection and prevention with graph-based method.
+ *
+ * It inherits/extends the BaseAIML for its implementation.
+ *
+ */
+
+class DrillingRearEvents extends BaseAIML
+{
+    constructor(pyFile, pyScriptPath, pyVersionPath, pyMode)
+    {
+        this.pyFile = pyFile;
+        this.pyScriptPath = pyScriptPath;
+        this.pyVersionPath = pyVersionPath;
+        this.pyMode = pyMode;
+    }
+    
+    
+    drillingRearEvents()
+    {
+        // call (Python-Shell call) to AIML codes, developed in Python
+        var pyFile              = this.pyFile;
+        var pyScriptPath        = this.pyScriptPath;
+        var pyVersionPath       = this.pyVersionPath;
+        var pyMode              = this.pyMode;
+        var cpml                =  new CallPthonMLCodes();
+        var CallPthonMLCodes    = require('./CallPthonMLCodes.js').CallPthonMLCodes;
+        var cpml = new CallPthonMLCodes();
+        cpml.callPythonMLScriptFromNodeJS(pyFile, pyScriptPath, pyVersionPath, pyMode);
+    }
+    
+    
+    testDrillingRearEvents()
+    {
+        /// call (Python-Shell call) to AIML codes, developed in Python
+        var pyFile              = this.pyFile;
+        var pyScriptPath        = this.pyScriptPath;
+        var pyVersionPath       = this.pyVersionPath;
+        var pyMode              = this.pyMode;
+        var cpml                =  new CallPthonMLCodes();
+        var CallPthonMLCodes    = require('./CallPthonMLCodes.js').CallPthonMLCodes;
+        var cpml = new CallPthonMLCodes();
+        cpml.callPythonMLScriptFromNodeJS(pyFile, pyScriptPath, pyVersionPath, pyMode);
+    }
+}
+
+module.exports = {DrillingRearEvents};
