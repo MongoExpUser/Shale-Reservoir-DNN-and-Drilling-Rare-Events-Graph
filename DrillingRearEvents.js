cb4eabd6edf72cdec7738bd36c2f97c10af07b15
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2019-08-22 21:18:39 -0500

    Update DrillingRearEvents.js

--- DrillingRearEvents.js
+++ DrillingRearEvents.js
@@ -21,9 +21,49 @@ class DrillingRearEvents extends BaseAIML
         this.pyMode = pyMode;
     }
     
-    drillingEventInsertRecordInMySQL(tableName)
+    drillingEventCreateTableInMySQL(tableName)
     {
 
+        var mySqlQuery = "CREATE TABLE IF NOT EXISTS " + String(tableName) +
+                            " (" + //primary key
+                            "DATA_ID INT AUTO_INCREMENT PRIMARY KEY, " +
+                            //data from regular drilling operation
+                            "ROP_fph DOUBLE, " +
+                            "RPM_rpm DOUBLE, " +
+                            "SPP_psi DOUBLE, " +
+                            "DWOB_lb DOUBLE, " +
+                            "SWOB_lb DOUBLE, " +
+                            "TQR_Ibft DOUBLE, " +
+                            "MUD_WEIGHT_sg DOUBLE, " +
+                            "MUD_VISC_cp DOUBLE, " +
+                            "MUD_FLOW_RATE_gpm DOUBLE, " +
+                            "BHA_TYPE_no_unit TEXT, " +
+                            //data from downhole MWD/LWD tool measurements
+                            "TVD_ft DOUBLE, " +
+                            "MD_ft DOUBLE, " +
+                            "INC_deg DOUBLE, " +
+                            "AZIM_deg DOUBLE, " +
+                            "CALIPER_HOLE_SIZE_inches DOUBLE, " +
+                            "GR_api DOUBLE, " +
+                            "DEEP_RESISTIVITY_ohm_m DOUBLE, " +
+                            "SHOCK_g DOUBLE, " +
+                            //event data
+                            "IS_VIBRATION BOOLEAN, " +
+                            "IS_KICK BOOLEAN, " +
+                            "IS_STUCKPIPE BOOLEAN, " +
+                            //time data
+                            "TIME_ymd_hms TEXT" +
+                            //constraints on some LWD data
+                            //"CHECK (0 >= GR_api <= 150), " +
+                            //"CHECK (0 >= DEEP_RESISTIVITY_ohm_m <= 2000)" +
+                        ")";
+            
+            return mySqlQuery;
+    }
+       
+            
+    drillingEventInsertRecordInMySQL(tableName)
+    {
         const value = true;
             
         if(value === true)
@@ -56,27 +96,9 @@ class DrillingRearEvents extends BaseAIML
                                 "TIME_ymd_hms)" +
                                 " VALUE (30, 300, 100, 350, 200, 95, 1.18, 3, 35.14, 'slick', 1000, 1200, 67, 110, 8.5, 50, 120, 20, FALSE, FALSE, FALSE, '10:22'" +
                             ")";
-                
-                
         }
             
         return mySqlQuery;
-        
-    }
-    
-    
-    drillingRearEvents()
-    {
-        // call AIML python script (this.pyFile) via Python-Shell
-        // pyFile (py_file_AIML.py) implements drilling rear events detection and prevention using graph method
-        var pyFile              = this.pyFile;
-        var pyScriptPath        = this.pyScriptPath;
-        var pyVersionPath       = this.pyVersionPath;
-        var pyMode              = this.pyMode;
-        var cpml                =  new CallPthonMLCodes();
-        var CallPthonMLCodes    = require('./CallPthonMLCodes.js').CallPthonMLCodes;
-        var cpml = new CallPthonMLCodes();
-        cpml.callPythonMLScriptFromNodeJS(pyFile, pyScriptPath, pyVersionPath, pyMode);
     }
     
     drillingRearEvents()
