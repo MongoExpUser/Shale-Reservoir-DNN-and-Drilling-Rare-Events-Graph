8c0c7dd5d2da194770c3046742d281d30a35e9fb
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2019-08-22 21:15:59 -0500

    Update DrillingRearEvents.js

--- DrillingRearEvents.js
+++ DrillingRearEvents.js
@@ -21,6 +21,64 @@ class DrillingRearEvents extends BaseAIML
         this.pyMode = pyMode;
     }
     
+    drillingEventInsertRecordInMySQL(tableName)
+    {
+
+        const value = true;
+            
+        if(value === true)
+        {
+            var mySqlQuery = "INSERT INTO " + String(tableName) +
+                                " (ROP_fph, " +
+                                "RPM_rpm, " +
+                                "SPP_psi, " +
+                                "DWOB_lb, " +
+                                "SWOB_lb, " +
+                                "TQR_Ibft, " +
+                                "MUD_WEIGHT_sg, " +
+                                "MUD_VISC_cp, " +
+                                "MUD_FLOW_RATE_gpm, " +
+                                "BHA_TYPE_no_unit, " +
+                                //data from downhole MWD/LWD tool measurements
+                                "TVD_ft, " +
+                                "MD_ft, " +
+                                "INC_deg, " +
+                                "AZIM_deg, " +
+                                "CALIPER_HOLE_SIZE_inches, " +
+                                "GR_api, " +
+                                "DEEP_RESISTIVITY_ohm_m, " +
+                                "SHOCK_g, " +
+                                //event data
+                                "IS_VIBRATION, " +
+                                "IS_KICK, " +
+                                "IS_STUCKPIPE, " +
+                                //time data
+                                "TIME_ymd_hms)" +
+                                " VALUE (30, 300, 100, 350, 200, 95, 1.18, 3, 35.14, 'slick', 1000, 1200, 67, 110, 8.5, 50, 120, 20, FALSE, FALSE, FALSE, '10:22'" +
+                            ")";
+                
+                
+        }
+            
+        return mySqlQuery;
+        
+    }
+    
+    
+    drillingRearEvents()
+    {
+        // call AIML python script (this.pyFile) via Python-Shell
+        // pyFile (py_file_AIML.py) implements drilling rear events detection and prevention using graph method
+        var pyFile              = this.pyFile;
+        var pyScriptPath        = this.pyScriptPath;
+        var pyVersionPath       = this.pyVersionPath;
+        var pyMode              = this.pyMode;
+        var cpml                =  new CallPthonMLCodes();
+        var CallPthonMLCodes    = require('./CallPthonMLCodes.js').CallPthonMLCodes;
+        var cpml = new CallPthonMLCodes();
+        cpml.callPythonMLScriptFromNodeJS(pyFile, pyScriptPath, pyVersionPath, pyMode);
+    }
+    
     drillingRearEvents()
     {
         // call AIML python script (this.pyFile) via Python-Shell 
