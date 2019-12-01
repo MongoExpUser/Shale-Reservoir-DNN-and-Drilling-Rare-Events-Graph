425e703d02c1b3e667aac728df22416426b4f28b
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2019-11-30 23:42:48 -0600

    Update ShaleReservoirApacheSpark.py

--- ShaleReservoirApacheSpark.py
+++ ShaleReservoirApacheSpark.py
@@ -38,8 +38,10 @@ try:
   from random import random, randint
   from unittest import TestCase, main
   from pyspark import SparkConf, SparkContext
+  from pyspark.ml.feature import VectorAssembler
   from pyspark.sql import SparkSession, DataFrame, types
-  from ShaleReservoir import ShaleDNN
+  from pandas import read_json, DataFrame as PythonDataFrame
+  from EcotertShaleReservoir import ShaleDNN
   #
   #all pyspark sub-modules: commment out - only here for referencing
   #import modules and sub modules, when necesary or required
@@ -77,8 +79,25 @@ class ShaleReservoirApacheSpark():
     self.separator()
   # End  __init__() method
   
+  
+  
+  def separator(self):
+    print("------------------------------------------------------------")
+  # End separator method()
+  
+  def duration_separator(self):
+    print("<===========================================================>")
+  # End duration_separator method()
+  
+  def get_spark_dataframe_shape(self, spark_dataframe_data):
+    count = 0
+    for index, item in enumerate(spark_dataframe_data):
+      count = index
+    return (count, len(spark_dataframe_data.columns))
+  # End get_spark_dataframe_shape() method
+  
   def calculate_stooip(self, total_number_of_reservoirs=None, engine_name=None):
-    self.separator(separator_type="long")
+    self.separator()
     print("{}{}".format(engine_name, "-based STOOIP computation started and in progress ....."))
     t0 = time.time()
     for each_number_of_reservoirs in range(total_number_of_reservoirs):
@@ -90,7 +109,7 @@ class ShaleReservoirApacheSpark():
     print("{}{}{}".format(engine_name, "-based STOOIP computation time (seconds):", '{0:.4f}'.format(duration)))
     self.duration_separator()
     print("STOOIP computation successfully completed.")
-    self.separator(separator_type="long")
+    self.separator()
   # End calculate_stooip() method
       
   def sample_one_stooip_calculation(self, total_number_of_reservoirs=None, spark_engine=True):
@@ -144,41 +163,111 @@ class ShaleReservoirApacheSpark():
       print("'TensorFlow' image classification successfully completed.")
   # End sample_two_machine_learning_with_tensorflow() method
   
-  def separator(self, separator_type=None):
-    if separator_type == "long":
-      print("------------------------------------------------------------")
+  def sample_three_read_json_reservoir_data_to_dataframe(self, json_data=None, nth_time=10, spark_engine=True):
+    
+    #define a dictionary/string containing reservoir data
+    data = { 'FIELD_NAME' : ['Yoho', 'Salabe', 'Agbabu', 'Owopele-north'],
+             'RESERVOIR_DEPTH_FT' : [12000.04, 1489.34, 400, 12567.43],
+             'FLUID_TYPE' : ['Light Oil', 'Condensate', 'Bitumen', 'Light oil'],
+             'FLUID_API' : [35.1, 45.3, 10.4, 34.4],
+             'FIELD_LOCATION' : ['Offshore', 'Swamp', 'Land', 'Land']
+    }
+    
+    #ensure data is in (or convert data to)  json format
+    json_data = dumps(data)
+   
+    print("Printing Contents of the JSON Representation:")
+    self.separator()
+    pprint(json_data)
+    self.separator()
+    
+    #to demo 3rd speed-up due to spark engine, load "JSON" data in a loop nth_time
+    if spark_engine:
+      #invoke loading of  'JSON' data in a loop with spike engine
+      engine_name = "Spark engine"
+      print("")
+      print("{}{}".format(engine_name, "-based loading of  'JSON' data in a loop started and in progress ....."))
+      sfc =  ShaleDNN()
+      #start spark by invoking SparkSession()
+      spark = SparkSession.builder.master("local").appName("Parallel and Distributed Data Demonstration").getOrCreate()
+      #read and create a spark dataframe from jSON data in a loop nth_times and print/pprint/show afterwards
+      t0 = time.time()
+      # 2. read and create in loop
+      for index in range(nth_time):
+        spark_dataframe_data = spark.read.json(spark.sparkContext.parallelize([json_data]))
+        #note: if json_file (.json file) is on disk, use: spark_dataframe_data = spark.read.json(../path_to_file/filename.json)
+      duration = time.time() - t0
+      self.duration_separator()
+      print("{}{}{}".format(engine_name, "-based loading of  'JSON' data in a loop time (seconds):", '{0:.4f}'.format(duration)))
+      self.duration_separator()
+      print("Printing Contents of the Spark DataFrame Representation:")
+      print("Check Data Type .....", type(spark_dataframe_data))
+      print("Schema .....")
+      spark_dataframe_data.printSchema()
+      print("All Data Rows with Shape:", self.get_spark_dataframe_shape(spark_dataframe_data))
+      self.separator()
+      spark_dataframe_data.show()
+      self.separator()
+      print("Loading of  'JSON' data in a loop successfully completed.")
+      #stop spark
+      spark.stop()
     else:
-      print("----------------------------")
-  # End separator method()
-  
-  def duration_separator(self):
-    print("<=================================================>")
-  # End duration_separator method()
-  
+      # #invoke loading of  'JSON' data in a loop without spike engine
+      engine_name = "Regular VM engine"
+      print("")
+      print("{}{}".format(engine_name, "-based loading of  'JSON' data in a loop started and in progress ....."))
+      t0 = time.time()
+      sfc =  ShaleDNN()
+      #read and create a pure python dataframe from jSON data in a loop nth_times and print/pprint/show afterwards
+      t0 = time.time()
+      for index in range(nth_time):
+        python_dataframe_data = PythonDataFrame(read_json(json_data))
+      duration = time.time() - t0
+      self.duration_separator()
+      print("{}{}{}".format(engine_name, "-based loading of  'JSON' data in a loop time (seconds):", '{0:.4f}'.format(duration)))
+      self.duration_separator()
+      print("Printing Contents of Pure Python DataFrame Representation:")
+      print("Data type .....", type(python_dataframe_data))
+      print("All Data Rows with Shape:", python_dataframe_data.head().shape)
+      self.separator()
+      pprint(python_dataframe_data.head())
+      self.separator()
+      print("Loading of  'JSON' data in a loop successfully completed.")
+      pass
+  # End test_sample_three_read_json_reservoir_data_to_dataframe() method
+#End ShaleReservoirApacheSpark() class
+
   
 class ShaleReservoirApacheSparkTest(TestCase):
   """ Test ShaleReservoirApacheSpark() class """
   
   def setUp(self):
     self.sras_demo = ShaleReservoirApacheSpark()
-    self.total_number_of_reservoirs = 1000000
+    self.total_number_of_reservoirs = 3000000
     self.spark_engine_yes = True
-    self.spark_engine_no = False
+    self.spark_engine_non = False
   # End setUp() method
     
-  def test_sample_one_stooip_calculation(self):
+  def _test_sample_one_stooip_calculation(self):
     print()
     #calculate stooip with and without spark engine
     self.sras_demo.sample_one_stooip_calculation(total_number_of_reservoirs=self.total_number_of_reservoirs, spark_engine=self.spark_engine_yes)
-    self.sras_demo.sample_one_stooip_calculation(total_number_of_reservoirs=self.total_number_of_reservoirs, spark_engine=self.spark_engine_no)
+    self.sras_demo.sample_one_stooip_calculation(total_number_of_reservoirs=self.total_number_of_reservoirs, spark_engine=self.spark_engine_non)
   #End test_sample_one_stooip_calculation() method
   
-  def test_sample_two_machine_learning_with_tensorflow(self):
+  def _test_sample_two_machine_learning_with_tensorflow(self):
     print()
     #run "TensorFlow" image classification example in "ShaleReservoir.py"
     self.sras_demo.sample_two_machine_learning_with_tensorflow(spark_engine=self.spark_engine_yes)
-    self.sras_demo.sample_two_machine_learning_with_tensorflow(spark_engine=self.spark_engine_no)
-  #End _test_sample_two_machine_learning_with_tensorflow() method
+    self.sras_demo.sample_two_machine_learning_with_tensorflow(spark_engine=self.spark_engine_non)
+  #End test_sample_two_machine_learning_with_tensorflow() method
+  
+  def test_sample_three_read_json_reservoir_data_to_dataframe(self):
+    print()
+    #run read_file
+    self.sras_demo.sample_three_read_json_reservoir_data_to_dataframe(json_data=None, nth_time=500, spark_engine=self.spark_engine_yes)
+    self.sras_demo.sample_three_read_json_reservoir_data_to_dataframe(json_data=None, nth_time=500, spark_engine=self.spark_engine_non)
+  #End test_sample_three_read_json_reservoir_data_to_dataframe() method
   
   def tearDown(self):
     print()
@@ -188,6 +277,6 @@ class ShaleReservoirApacheSparkTest(TestCase):
     self.spark_engine_no = None
   # End tearDown() method
 #End ShaleReservoirApacheSparkTest() class
-    
+
 # invoke test
 main(verbosity=2)
