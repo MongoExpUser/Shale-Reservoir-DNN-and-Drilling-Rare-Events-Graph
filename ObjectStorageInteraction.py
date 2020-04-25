5748ab073de818084a66e22a5ce1698b6628eca6
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2020-04-25 11:54:13 -0500

    Update ObjectStorageInteraction.py

--- ObjectStorageInteraction.py
+++ ObjectStorageInteraction.py
@@ -27,6 +27,18 @@
 # *******************************************************************************************************************************************
 # *******************************************************************************************************************************************
 
+try:
+    """  import commonly used modules and check for import error
+    """
+    #import
+    import sys, boto3, s3fs
+    from pprint import pprint
+    from boto3.session import Session
+    from botocore.client import Config
+    #check for error
+except(ImportError) as err:
+  print(str(err))
+
 
 class ObjectStorageInteraction():
     """
@@ -71,9 +83,13 @@ class ObjectStorageInteraction():
         # for object in bucket.objects.all():
             # print(object.key)
         # print(client.list_buckets())
-        # NOTE: for BIG_DATA analytics: the "client.select_object_content()" is useful for querying CSV, JSON and PARQUET files directly
-        # with simple SQL expressions on object storage, essentially turning "data-lake" into serverless database - no need to move data to DBRMS
-        # see - https://aws.amazon.com/blogs/storage/querying-data-without-servers-or-databases-using-amazon-s3-select/
+        # NOTE: for BIG_DATA analytics:
+        # 1) the "client.select_object_content()" is useful for querying CSV, JSON and PARQUET files directly with SQL
+        #    expressions on S3, essentially turning "data-lake" into serverless database - no need to move data to DBRMS
+        #    see - https://aws.amazon.com/blogs/storage/querying-data-without-servers-or-databases-using-amazon-s3-select/
+        # 2) Alternatively, on S3 and other S3-compatible object storage systems; provided by linode, GCP, etc; PySpark can also
+        #    be used to load CSV, JSON and PARQUET files as DataFrames and "PySpark SQL" can then be used to issue SQL expressions
+        #.   against the DataFrames just like "client.select_object_content()".
         return {"bucket": bucket, "client": client}
     # End object_storage_interaction_using_boto3() method
 
